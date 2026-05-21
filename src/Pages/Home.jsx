e083e90891eb6e18a24ab686a94d222ff1f8090d
01Kiruthika <kiruthikasundaram2004@gmail.com>
2026-05-21 11:51:56 +0530

    fully  complete

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import './Home.css'
 import Icons from '../Components/Icons'
 import { useNavigate } from 'react-router-dom'
+import myphoto from "../Images/myphoto.png"
 const Home = () => {
   const navigate = useNavigate()
   return (
     <div className='homecontainer'>
       <div className="about-me">
         <div className="my-image">
-          <img src="https://www.shutterstock.com/image-photo/beauty-charisma-head-shot-portrait-600nw-2647728057.jpg" alt="my image" />
+          <img src={myphoto} alt="my image" />
         </div>
         <div className="about-content">
           <h2>Hi,</h2>
